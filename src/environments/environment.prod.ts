3daf1b77f10d2ddc59aec5c0c066384d3c348ee3
sarathy <sarathy0101@gmail.com>
2021-06-02 02:29:15 +0530

    add cred in prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBj83J9laPKO3aE_-Cj4XoZPPmSKmDKs60",
+    authDomain: "blog-3693f.firebaseapp.com",
+    projectId: "blog-3693f",
+    storageBucket: "blog-3693f.appspot.com",
+    messagingSenderId: "269926240689",
+    appId: "1:269926240689:web:a18706aa9194efd3f22d90",
+    measurementId: "G-EVX1VV8GBM"
+  }
 };
